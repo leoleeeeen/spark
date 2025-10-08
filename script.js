65cb4ff146eeb474e210aeb9d977fab466215fa7
leoleeeeen <leoleeeeen@gmail.com>
2025-10-08 20:06:08 +0300

    fix mobile view

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const burger = document.querySelector('.burger')
-const cross = document.querySelector('.cross')
-const menu = document.querySelector('.mobile-header')
+const burger = document.getElementById('burger')
+const cross = document.getElementById('cross')
+const menu = document.getElementById('mobile-header')
+const header_banner = document.getElementById('hero-section');
+const main = document.querySelector('body');
 
 burger.addEventListener('click', () => {
     menu.style.display = 'block';
+    header_banner.style.display = 'none';
+    main.classList = 'disable-scroll';
 })
 
 cross.addEventListener('click', () => {
     menu.style.display = 'none';
+    header_banner.style.display = 'flex';
+    main.classList = '';
 })
